6b82197775a10a3afdf8b9d71aced987f3d94efe
nkrce <nikolina.krce@gmail.com>
2023-07-06 03:39:07 +0200

    prvi commit

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+   <div class="about">
+    <h1>Log in form: </h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control"
+id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control"
+id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <button type="submit" class="btn btn-primary">Log in</button>
+            <small id="emailHelp" class="form-text text-muted" >Don't have an account? <a href="/signup">Sign up</a></small>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/Signup.vue
@@ -0,0 +1,37 @@
+<template>
+   <div class="about">
+    <h1>Sign up form: </h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Name</label>
+              <input type="password" class="form-control"
+id="exampleInputPassword1" placeholder="Name" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control"
+id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control"
+id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Confirm password</label>
+              <input type="password" class="form-control"
+id="exampleInputPassword1" placeholder="Confirm password" />
+            </div>
+            <button type="submit" class="btn btn-primary">Sign up</button>
+            <small id="emailHelp" class="form-text text-muted" >Already have an account? <a href="/login">Log in</a></small>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
