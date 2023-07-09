2fd38ab566fe51082f3ef0ccda1b41ad42724bde
nkrce <nikolina.krce@gmail.com>
2023-07-09 18:50:56 +0200

    dodan start page

---
+++ src/components/StartPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <p>
+      Simple and easy way <br>
+      to order flowers ✿
+    </p>
+ 
+    <router-link to="/login"><button type="submit" class="btn btn-primary">Log in</button></router-link> <br> <br>
+    <router-link to="/signup"><button type="submit" class="btn btn-primary">Sign up</button></router-link>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
