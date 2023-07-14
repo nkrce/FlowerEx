2cb34679c3fd70d1bda5bab612e32313a4ecf361
nkrce <nikolina.krce@gmail.com>
2023-07-14 22:16:49 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="FlowerEx logo" src="../assets/logo1.png" style="width:200px;height:200px;">
-    <HelloWorld msg="FlowerEx"/>
+    <HelloWorld msg="FlowerEx" style="color:white"/>
   </div>
 </template>
 
