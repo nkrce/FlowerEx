6b9e41862d4f5afeebfab88f6d5faaef4681f5c6
nkrce <nikolina.krce@gmail.com>
2023-07-14 20:37:59 +0200

    dodan MainPage.vue

---
+++ src/views/MainPage.vue
@@ -0,0 +1,7 @@
+<template>
+   <div class="about">
+    <h2>Welcome back! </h2>
+    <p> Choose a store and order flowers you like <br>
+        for yourself  or whoever else you'd like! </p>
+   </div>
+</template>
