ccbb861c067838e269a09c2cd687d87a38bd0626
nkrce <nikolina.krce@gmail.com>
2023-08-26 21:15:45 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/signup">Sign up</router-link> |
-    <router-link to="/login">Log in</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
