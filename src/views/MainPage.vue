3cfb3d328c9efe1a83bba08284159e79c07eac1f
nkrce <nikolina.krce@gmail.com>
2023-08-27 13:58:38 +0200

    Update MainPage.vue

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,4 +1,9 @@
 <template>
+<nav class="navbar navbar-light" style="background-color: #885AEB;">
+  <a class="navbar-brand" href="#">
+    <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+  </a>
+</nav>
    <div class="about">
     <h2>Welcome back! </h2>
     <p> Choose a store and order flowers you like <br>
