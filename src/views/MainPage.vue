82115cb3dfb4f79296b53fb19179043d3f121830
nkrce <nikolina.krce@gmail.com>
2023-08-31 17:48:26 +0200

    Update MainPage.vue

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,8 +1,12 @@
 <template>
 <nav class="navbar navbar-light" style="background-color: #885AEB;">
-  <a class="navbar-brand" href="#">
-    <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+  <a class="navbar-brand" href="/settings">
+    <img src="../assets/settings.png" width="30" height="30" alt="">
   </a>
+
+   <a class="navbar-brand ml-auto" href="/history">
+      <img src="../assets/history.png" width="30" height="30" alt="">
+    </a>
 </nav>
    <div class="about">
     <h2>Welcome back! </h2>
