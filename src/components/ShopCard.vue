7dcc0632abf9d1073f5beb52dee0dec15499a5e5
nkrce <nikolina.krce@gmail.com>
2023-09-01 01:17:15 +0200

    shop card komponent

---
+++ src/components/ShopCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <img :src="imageSource" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">{{ description }}</p>
+      <a :href="link" class="btn btn-light" style="background-color: #b9a3e9;">{{ buttonText }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageSource: String,
+    title: String,
+    description: String,
+    link: String,
+    buttonText: String
+  }
+};
+</script>
