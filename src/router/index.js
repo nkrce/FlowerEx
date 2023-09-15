d88cf85b79ac318a77ded0ee943a1a062c643e19
nkrce <nikolina.krce@gmail.com>
2023-09-16 00:56:57 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../MainPage.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/MainPage.vue')
+  },
+  {
+    path: '/flowershop',
+    name: 'Flower shop',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/FlowerShop.vue')
   }
 ]
 
