1811ddcb26ead48867e9b2393a71f159f3546b06
nkrce <nikolina.krce@gmail.com>
2023-09-17 15:07:11 +0200

    update History.vue

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,10 +1,20 @@
 <template>
-<nav class="navbar navbar-light" style="background-color: #885AEB;">
-  <a class="navbar-brand" href="#">
+<nav class="navbar navbar-light" style="background-color: #C358B9;">
+  <a class="navbar-brand" href="/mainpage">
     <img src="../assets/back.png" width="30" height="30" alt="">
   </a>
 </nav>
    <div class="about">
-    <h2>Order history ✿</h2>
+    <h2 style="color:white">Order history ✿</h2>
    </div>
 </template>
+
+<style scoped>
+.about {
+  background:#C358B9;
+  width: 100%;
+  height: 10vh;
+  border-bottom-left-radius: 100px; 
+  border-bottom-right-radius: 100px;
+}
+</style>
